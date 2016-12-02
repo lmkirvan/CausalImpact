c3275716ddc5fe444d6cb899c58f787cf77a5076
Billy Everyteen <kyle@datascience.com>
2016-12-02 07:33:58 -0800

    Switching canonical example from Sia to Courtney Barnett to match the episode

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -14,12 +14,12 @@ shinyUI(fluidPage(theme = "bootstrap.css",
   ),
   sidebarLayout(
     sidebarPanel(
-      textInput('y_article', 'Article:', 'Sia'),
-      textInput('x1_article', 'Control Article 1:', 'Dragonette'),
-      textInput('x2_article', 'Control Article 2:', 'PJ Harvey'),
-      textInput('x3_article', 'Control Article 3:', 'Serena Ryder'),
-      dateRangeInput('pre_period', 'Pre-period:', start = '2015-10-01', end = '2015-11-06'),
-      dateRangeInput('post_period', 'Post-period:', start = '2015-11-07', end = '2015-11-14'),
+      textInput('y_article', 'Article:', 'Courtney Barnett'),
+      textInput('x1_article', 'Control Article 1:', 'Joshua Tillman'),
+      textInput('x2_article', 'Control Article 2:', 'Sleater Kinney'),
+      textInput('x3_article', 'Control Article 3:', 'Sharon Van Etten'),
+      dateRangeInput('pre_period', 'Pre-period:', start = '2016-05-13', end = '2016-05-21'),
+      dateRangeInput('post_period', 'Post-period:', start = '2016-05-22', end = '2016-05-27'),
       actionButton("submit", "Run analysis")
     ),
     mainPanel(
