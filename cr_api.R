136ec9775e6a356731a8470e7f06c77768382305
lmkirvan <kirvan.lewis@gmail.com>
2016-12-11 10:30:44 -0500

    General function for downloading csv from consumerfinance.gov

---
+++ cr_api.R
@@ -0,0 +1,30 @@
+
+get_complaints <- function(api_endpoint = "https://data.consumerfinance.gov/resource/jhzv-w97w.csv?$$app_token=%s&$limit=50000", 
+                           selects = NULL, 
+                           filters = NULL,
+                           token ="VWJa6qhKHXoOyZiCNnCRlbbNX"){
+  base_url <- sprintf(api_endpoint,token)
+  count <- read.csv(file = paste(base_url, "&$select=count(complaint_id)", sep = ""),
+                    stringsAsFactors = F)
+  count <- count[1,1]
+  options(scipen = 999)
+  pages <- as.character(seq(from = 0 , to = count, by = 50000))
+  df <- data.frame()
+  for(i in 1:length(pages)){
+    temp <- read.csv(file = paste(base_url, 
+                                  offset = paste("&$offset=", pages[i], sep = ""),
+                                  selects, 
+                                  filters, 
+                                  sep = ""),
+                     stringsAsFactors = F)
+    df <- rbind(df, temp)
+  }
+  df
+}
+
+
+df <- get_complaints(selects = "&$select=company,product")
+
+companies <- unique(df$company)
+products <- unique(df$product)
+rm(df)
